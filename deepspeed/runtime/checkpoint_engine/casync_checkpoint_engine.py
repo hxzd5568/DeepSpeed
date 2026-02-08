632f16c6cf2cb221c96c5211cd92d5d9b7a67159
hxzd5568 <3257591325@qq.com>
2026-02-08 08:12:40 +0000

    [fit into zero3]

--- deepspeed/runtime/checkpoint_engine/casync_checkpoint_engine.py
+++ deepspeed/runtime/checkpoint_engine/casync_checkpoint_engine.py
@@ -8,8 +8,41 @@
 from deepspeed.runtime.checkpoint_engine.checkpoint_engine import \
     CheckpointEngine, CheckpointCommitInfo
 import time
+# import torch
+
 ENGINE_NAME = "CasyncEngine"
 
+
+# def profile_tensor(state_dict):
+#     all_max, all_p_min, all_min, all_abs_mean = [], [], [], []
+    
+#     def collect_stats(obj):
+#         if isinstance(obj, dict):
+#             for v in obj.values():
+#                 collect_stats(v)
+#         elif isinstance(obj, list):
+#             for item in obj:
+#                 collect_stats(item)
+#         elif isinstance(obj, torch.Tensor) and obj.numel() > 1200:
+#             t = obj.detach().float()
+#             all_max.append(t.max().item())
+#             all_min.append(t.min().item())
+#             all_p_min.append(t.abs().min().item())
+#             all_abs_mean.append(t.abs().mean().item())
+#             if t.abs().mean().item() > 0.01:
+#                 print(obj)
+#         else:
+#             pass
+    
+#     collect_stats(state_dict)
+#     if all_max:
+#         print(f"[PROFILE] max: {sum(all_max)/len(all_max):.6f}, "
+#               f"[PROFILE] positive min: {sum(all_p_min)/len(all_min)}, "
+#               f"[PROFILE] min: {sum(all_min)/len(all_min):.6f}, "
+#               f"[PROFILE] abs_mean: {sum(all_abs_mean)/len(all_abs_mean):.6f}")
+#         look_up_sign = sum(all_abs_mean)/len(all_abs_mean)
+#     else:
+#         print("[PROFILE] No tensors found in optimizer state.")
 
 class CasyncEngine(CheckpointEngine):
 
@@ -35,19 +68,10 @@ class CasyncEngine(CheckpointEngine):
 
     def save(self, state_dict, path: str):
         # return self.ckpt_engine.coalition_save(state_dict, path)
-        if 'model_states' in path:
-            self.ckpt_engine.coalition_save(state_dict, path)
-        else:
-            # return
-            self.ckpt_engine.save(state_dict, path)
-
+        self.ckpt_engine.coalition_save(state_dict, path)
 
     def load(self, path: str, map_location=None):
-        if 'model_states' in path:
-            return self.ckpt_engine.split_load_(path, map_location)
-        else:
-            return self.ckpt_engine.load(path, map_location)
-
+        return self.ckpt_engine.split_load(path, map_location)
     
     def wait(self, persist=True):
         self.ckpt_engine.wait(persist=persist)
--- deepspeed/runtime/checkpoint_engine/torch_checkpoint_engine.py
+++ deepspeed/runtime/checkpoint_engine/torch_checkpoint_engine.py
@@ -34,6 +34,7 @@ class TorchCheckpointEngine(CheckpointEngine):
 
     def load(self, path: str, map_location=None):
         log_dist(f"[Torch] Begin Load checkpoint from {path}...", ranks=[0])
+        # raise ValueError("TRACE++++++++++++++++++++++++++++++")
         partition = torch.load(path, map_location=map_location, weights_only=False)
         log_dist(f"[Torch] End Load checkpoint from {path}...", ranks=[0])
         return partition
